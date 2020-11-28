2e991879b43e4f19246462ac92509ca2108dd46f
Baptiste Garcin <baptiste.garcin@gmail.com>
2020-11-28 16:27:10 +0100

    Update DummyComponent.js

--- DummyComponent.js
+++ DummyComponent.js
@@ -11,9 +11,9 @@ class DummyComponent extends React.Component {
 
   handleChange(event) {
     this.setState({
-      // Ici, c'est une syntax un peu particulière, elle permet de définir dynamiquement la clé d'un objet.
+      // Ici, c'est une syntaxe un peu particulière, elle permet de définir dynamiquement la clé d'un objet.
       // Selon que la fonction est appelée à partir de l'input password ou email, event.target.name sera "email" ou "password"
-      // Et donc le bon champ sera mis à jour. Avec cette technique, tu as uen seule fonction pour tous tes champs.
+      // Et donc le bon champ sera mis à jour. Avec cette technique, tu as une seule fonction pour tous tes champs.
       [event.target.name]: event.target.value,
     })
   }
